63e8be2ba89f28094dc9614e16978b4b19065bcd
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-08-17 12:04:41 +0200

    Creating base

---
+++ src/components/Formulaire.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+
+class Formulaire extends Component {
+  state = {
+    message: '',
+    length: this.props.length
+  }
+
+  createMessage = () => {
+    const { addMessage, pseudo, length } = this.props
+
+    const message = {
+      pseudo,
+      message: this.state.message
+    }
+
+    addMessage(message)
+
+    // Reset
+    this.setState({ message: '', length })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    this.createMessage()
+  }
+
+  handleChange = event => {
+    const message = event.target.value
+    const length = this.props.length - message.length
+    this.setState({ message, length })
+  }
+
+  handleKeyUp = event => {
+    if (event.key === 'Enter') {
+      this.createMessage()
+    }
+  }
+
+  render () {
+    const { length, message } = this.state
+
+    return (
+      <form
+        className='form'
+        onSubmit={this.handleSubmit}
+      >
+        <textarea
+          value={message}
+          onChange={this.handleChange}
+          onKeyUp={this.handleKeyUp}
+          required
+          maxLength={this.props.length}
+        />
+        <div className='info'>
+          {length}
+        </div>
+        <button type='submit'>
+          Envoyer !
+        </button>
+      </form>
+    )
+  }
+}
+
+export default Formulaire
