d9b957ac5bd85933675eefb1f5448e64529649ee
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-08-19 10:07:03 +0200

    Firebase & user experience

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
-const Message = () => {
-  return (
-    <p className='user-message'>
-      Mon message
-    </p>
-  )
+const Message = ({ content, pseudo, pseudoLog }) => {
+  if (pseudo === pseudoLog) {
+    return (
+      <p className='user-message'>
+        {content}
+      </p>
+    )
+  } else {
+    return (
+      <p className='not-user-message'>
+        <strong>{pseudo} :</strong> {content}
+      </p>
+    )
+  }
 }
 
 export default Message
